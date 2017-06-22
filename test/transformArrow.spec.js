a26379865206d4cf15f8fea5842d66ad8c7e4003
sarahlim <sarah@khanacademy.org>
2017-06-21 21:33:26 -0700

    Add passing tests for block arrow functions and async arrow functions

--- test/transformArrow.spec.js
+++ test/transformArrow.spec.js
@@ -35,17 +35,110 @@ if (true) {
     });
 
     it('handles block arrow functions', function () {
-        // TODO: No arguments
-        // TODO: Multiple arguments
+        // No arguments.
+        assert.equal(
+            transformArrow(
+`const x = 4;
+const f = () => {
+    return 1 + 1;
+};
+f() + x;`
+            ),
+`const x = 4;
+const f = function () {
+    return 1 + 1;
+};
+f() + x;`
+        );
+
+        // Multiple arguments.
+        assert.equal(
+            transformArrow(
+`let addTwoMaybe = null;
+if (true) {
+    addTwoMaybe = (x, y) => {
+        const z = 10;
+        return x + y + 10;
+    };
+}`
+            ),
+`let addTwoMaybe = null;
+if (true) {
+    addTwoMaybe = function (x, y) {
+        const z = 10;
+        return x + y + 10;
+    };
+}`
+        );
     });
 
     it('handles async inline arrow functions', function () {
-        // TODO: No arguments
-        // TODO: Multiple arguments
+        // No arguments.
+        assert.equal(
+            transformArrow(
+`const x = 4;
+const f = async () => 1 + 1;
+f() + x;`
+            ),
+`const x = 4;
+const f = async function () {
+    return 1 + 1;
+};
+f() + x;`
+        );
+
+        // Multiple arguments.
+        assert.equal(
+            transformArrow(
+`let addTwoMaybe = null;
+if (true) {
+    addTwoMaybe = async (x, y) => x + y;
+}`
+            ),
+`let addTwoMaybe = null;
+if (true) {
+    addTwoMaybe = async function (x, y) {
+        return x + y;
+    };
+}`
+        );
     });
 
     it('handles async block arrow functions', function () {
-        // TODO: No arguments
-        // TODO: Multiple arguments
+        // No arguments.
+        assert.equal(
+            transformArrow(
+`const x = 4;
+const f = async () => {
+    return 1 + 1;
+};
+f() + x;`
+            ),
+`const x = 4;
+const f = async function () {
+    return 1 + 1;
+};
+f() + x;`
+        );
+
+        // Multiple arguments.
+        assert.equal(
+            transformArrow(
+`let addTwoMaybe = null;
+if (true) {
+    addTwoMaybe = async (x, y) => {
+        const z = 10;
+        return x + y + 10;
+    };
+}`
+            ),
+`let addTwoMaybe = null;
+if (true) {
+    addTwoMaybe = async function (x, y) {
+        const z = 10;
+        return x + y + 10;
+    };
+}`
+        );
     });
 });
